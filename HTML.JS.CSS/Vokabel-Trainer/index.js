7073240fa399bf52ee32b383528fa111b9ef3c1a
SchluckyLuke <matthaeus.h24@gmail.com>
2026-02-27 22:21:17 +0100

    Initial commit

---
+++ HTML.JS.CSS/Vokabel-Trainer/index.js
@@ -0,0 +1,11 @@
+const submitBtn = document.getElementById("submitBtn");
+const languageInputGer = document.getElementById("languageInputGer");
+
+function updateLang(){
+    if(languageInputGer.checked){
+        console.log("cool");
+    }
+    else{
+        console.log("uncool");
+    }
+}
---
+++ Python/smth.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+c = tk.Canvas()
+
+c.create_rectangle(100, 200, 200, 300, fill="red")
+
+root.mainloop()
